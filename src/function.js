2dfabf5b61cd8bfc76663e1301dcc5df9fbaad05
gwen <gwendoline.garlet@live.fr>
2024-03-04 13:19:35 +0100

    last commit

--- src/function.js
+++ src/function.js
@@ -1,46 +1,53 @@
 const token = "M. Mime";
-const sanitizedToken = token.replace(/\s/g, ''); // Supprimer les espaces HTML du jeton
-console.log(sanitizedToken);
+// Supprimer les espaces HTML du jeton
+const sanitizedToken = token.replace(/\s/g, "");
 
-export function createCard(name,types,srcImg,id){
-    let card = document.createElement('div');
-    card.setAttribute('id',id)
-    let typess = document.createElement('div');
-    if(name == token || name  == "Mime Jr."){
-        card.classList.add(sanitizedToken);
-    }else{
-        
-        card.classList.add('card',name.toLowerCase());
-    }
-    card.setAttribute('data-id', id);
-    typess.classList.add('types-list');
+// Fonction pour créer une card
+export function createCard(name, types, srcImg, id) {
+  // Création de la balise div
+  let card = document.createElement("div");
+  card.setAttribute("id", id);
+  let typess = document.createElement("div");
 
-    let title = document.createElement('h3');
-    title.textContent = name;
+  // Verification de M. Mime Car j'avais une erreur sur celui la
+  if (name == token || name == "Mime Jr.") {
+    card.classList.add(sanitizedToken);
+  } else {
+    card.classList.add("card", name.toLowerCase());
+  }
+  card.setAttribute("data-id", id);
+  typess.classList.add("types-list");
 
-    let image = document.createElement('img');
-    image.setAttribute('src', srcImg);
-    image.classList.add('img-pokemon-list');
-    
+  // Création de la balise titre h3
+  let title = document.createElement("h3");
+  // Ici j'utilise textContent pour éviter les injections SQL
+  title.textContent = name;
 
-    types.forEach(type => {
-        let p = document.createElement('p');
-        p.textContent = type.name;
-        p.classList.add('type');
-        typess.append(p);
-        p.style.background = colours[type.name.toLowerCase()]
-    })
+  // Création de la balise img
+  let image = document.createElement("img");
+  image.setAttribute("src", srcImg);
+  image.classList.add("img-pokemon-list");
 
-    card.append(title);
-    card.append(image);
-    card.insertAdjacentElement('beforeend',typess)
+  // Pour chaque type je créer une balise p
+  types.forEach((type) => {
+    let p = document.createElement("p");
+    p.textContent = type.name;
+    p.classList.add("type");
+    typess.append(p);
+    p.style.background = colours[type.name.toLowerCase()];
+  });
 
-    return card;
+  // J'ajoute toutes ces balises créé à la card
+  card.append(title);
+  card.append(image);
+  card.insertAdjacentElement("beforeend", typess);
+
+  return card;
 }
 
-export function cardPokemon(name,types,srcImg,hp,atack,def,evolution){
-
-    let template =  `<div class="card-pokemon">
+// Une autre façons de créer une card avec un template
+export function cardPokemon(name, types, srcImg, hp, atack, def, evolution) {
+  let template = `<div class="card-pokemon">
      <header class="head-pokemon ">
        <h3 class="pokemon-title">${name}</h3>
        <span id="close">
@@ -67,13 +74,12 @@ export function cardPokemon(name,types,srcImg,hp,atack,def,evolution){
         <tr >
             <th>Evolution</th>
             <td class="evolution" >
-            `
-            for(let evo of evolution){
-              template += evo.name
-                console.log(evo.name);
-            }
-            template += 
-            `
+            `;
+  for (let evo of evolution) {
+    template += evo.name;
+    console.log(evo.name);
+  }
+  template += `
             </td>
  
         </tr>
@@ -81,47 +87,52 @@ export function cardPokemon(name,types,srcImg,hp,atack,def,evolution){
     
     </table>
     <div class="types">
-    `
-    for(let type of types){
-      template += `<p  style="background: ${colours[type.name.toLowerCase()]};">${type.name}</p>`
-    }
-    template += `
+    `;
+  for (let type of types) {
+    template += `<p  style="background: ${colours[type.name.toLowerCase()]};">${
+      type.name
+    }</p>`;
+  }
+  template += `
     </div>
     </div>
     </div>
-     </div>`
-   return template
+     </div>`;
+  return template;
 }
 
-export  function classSwitch(element,classAdd, classRemove){
-    element.addClass(classAdd)
-    element.removeClass(classRemove);
+// Fonction pour switcher les classes
+export function classSwitch(element, classAdd, classRemove) {
+  element.addClass(classAdd);
+  element.removeClass(classRemove);
 }
 
+// Fonction de filtre
 export function filtreTexte(arr, requete) {
-    return arr.filter(function (el) {
-      return el.name.toLowerCase().indexOf(requete.toLowerCase()) !== -1;
-    })
+  // Je retourne une fonction de filtre
+  return arr.filter(function (el) {
+    return el.name.toLowerCase().indexOf(requete.toLowerCase()) !== -1;
+  });
 }
 
-
+// Initialisation des couleurs
 const colours = {
-    normal: '#A8A77A',
-    feu: '#EE8130',
-    eau: '#6390F0',
-    électrik : '#F7D02C',
-    plante: '#7AC74C',
-    glace : '#96D9D6',
-    combat : '#C22E28',
-    poison: '#A33EA1',
-    sol: '#E2BF65',
-    vol: '#A98FF3',
-    psy: '#F95587',
-    insecte: '#A6B91A',
-    roche: '#B6A136',
-    spectre: '#735797',
-    dragon: '#6F35FC',
-    dark: '#705746',
-    acier: '#B7B7CE',
-    fée : '#D685AD',
+  normal: "#A8A77A",
+  feu: "#EE8130",
+  eau: "#6390F0",
+  électrik: "#F7D02C",
+  plante: "#7AC74C",
+  glace: "#96D9D6",
+  combat: "#C22E28",
+  poison: "#A33EA1",
+  sol: "#E2BF65",
+  vol: "#A98FF3",
+  psy: "#F95587",
+  insecte: "#A6B91A",
+  roche: "#B6A136",
+  spectre: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  acier: "#B7B7CE",
+  fée: "#D685AD",
 };
--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// Importation des fonctions du fichier function.js
 import {
   createCard,
   cardPokemon,
@@ -5,7 +6,8 @@ import {
   filtreTexte,
 } from "./function.js";
 
-// Cette fonction permet de s'assurer que tout ce qui se trouve à l'intérieur sera exécuté une fois le document HTML chargé.
+// Cette fonction permet de s'assurer que tout ce qui se trouve à l'intérieur
+// sera exécuté une fois le document HTML chargé.
 $(document).ready(function () {
   // Un petit système de loading
   $(".body").removeClass("in-active");
@@ -16,22 +18,66 @@ $(document).ready(function () {
   const nbPokemon = 100;
   const url = `https://pokebuildapi.fr/api/v1/pokemon/limit/${nbPokemon}`;
 
+  // Requête à l'API des pokemons
+  $.ajax({
+    // Option de la requête
+    type: "GET",
+    url: url,
+    dataType: "json",
+    success: (data) => {
+      // Récupération des pokémons
+      dataPokemon = data;
+
+      // Selection de l'input de recherche
+      let inputSearch = document.querySelector("#searchPokemon");
+      inputSearch.addEventListener("input", (e) => {
+        // On filtre la recherche avec la fonction filtreTexte()
+        let searchInputPokemon = filtreTexte(dataPokemon, e.target.value);
+
+        // Rendu de la recherche
+        renderCards(searchInputPokemon);
+        // Reset de la recherche
+        $("#btnReset").click(function () {
+          renderCards(dataPokemon);
+        });
+      });
+
+      // Rendu de l'API
+      renderCards(dataPokemon);
+    },
+    error: () => {
+      console.log("error");
+    },
+  });
+
+  // Fonction de rendu des cards pokemons
   function renderCards(pokemonData) {
+    // Selection du main avec Js
     let main = document.querySelector("main");
     main.innerHTML = "";
 
+    // Création d'une card pour chaque pokemon
     pokemonData.forEach((data) => {
       let card = createCard(data.name, data.apiTypes, data.image, data.id);
+      // Ajouter les cards au main
       main.append(card);
 
+      // Ajouter un évenement au click à chaque card créer
       $(card).click(function (e) {
         e.preventDefault();
-        $("form").addClass("in-active");
         let container = $(".pokemon");
+        $("form").addClass("in-active");
+
+        // Utilisation de classSwitch() pour "cacher" le main
+        // et laisser apparaitre l'item du pokemon clické
         classSwitch($("main"), "in-active", "active");
         classSwitch($(".pokemon"), "active", "in-active");
 
+        // Récupération de l'id du pokémon
         let cardId = Number(this.id);
+
+        // Utilisation de find() pour chercher dans pokemonData
+        // si pokemon clické existe
         let selectedPokemon = pokemonData.find(
           (pokemon) => pokemon.id === cardId
         );
@@ -47,6 +93,7 @@ $(document).ready(function () {
         );
         container.append(cardDetails);
 
+        // Fermer le detail
         $("#close").click(function () {
           $(".pokemon").empty();
           classSwitch($(".pokemon"), "in-active", "active");
@@ -56,31 +103,6 @@ $(document).ready(function () {
       });
     });
   }
-
-  // Requête à l'API des pokemons
-  $.ajax({
-    type: "GET",
-    url: url,
-    dataType: "json",
-    success: (data) => {
-      dataPokemon = data;
-
-      let inputSearch = document.querySelector("#searchPokemon");
-      inputSearch.addEventListener("input", (e) => {
-        let searchInputPokemon = filtreTexte(dataPokemon, e.target.value);
-
-        renderCards(searchInputPokemon);
-        $("#btnReset").click(function () {
-          renderCards(dataPokemon);
-        });
-      });
-
-      renderCards(dataPokemon);
-    },
-    error: () => {
-      console.log("error");
-    },
-  });
 
   // Gestion du formulaire de recherche
   $("form").on("submit", function (event) {
